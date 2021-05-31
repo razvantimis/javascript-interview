44ee08e37dcfb54dccf2f0c643ae7f296cd8e0e2
Timis Dev <contact@timis.dev>
2021-05-31 16:49:04 +0300

    Add

--- 03_question-closure.js
+++ 03_question-closure.js
@@ -6,60 +6,65 @@
 // As we know, each function is object in Javascript
 
 // Exemple 1
-function test() {
-  var myGlobal = 10;
-  var fn1 = function () {
-    var inner = 2;
-    return inner + myGlobal;
+function ex1() {
+  function test() {
+    var myGlobal = 10;
+    var fn1 = function () {
+      var inner = 2;
+      return inner + myGlobal;
+    }
+
+    console.dir(fn1)
   }
 
-  console.dir(fn1)
+  test()
 }
-
-test()
 
 // Exemple 2 ( the clasic one when we return a function )
-
-function add(number1) {
-  let privateVar = 10;
-  function innerAdd(number2) {
-    return number1 + number2 + privateVar
+function ex2() {
+  function add(number1) {
+    let privateVar = 10;
+    function innerAdd(number2) {
+      return number1 + number2 + privateVar
+    }
+    console.log(innerAdd)
+    return innerAdd;
   }
-  console.log(innerAdd)
-  return innerAdd;
-}
 
-const addInner = add(1);
-console.log(addInner(2))
+  const addInner = add(1);
+  console.log(addInner(2))
+
+}
 
 // Exemple 3
 // Closure mistake in combination with var
-
-function showHelp(help) {
-  console.log(help)
-}
-
-function makeShowHelp(help) {
-  return function () {// here we put help param in closures
-    showHelp(help);
+function ex3() {
+  function showHelp(help) {
+    console.log(help)
   }
-}
 
-function setupHelp() {
-  var helpText = [
-    { 'id': 'email', 'help': 'Your e-mail address' },
-    { 'id': 'name', 'help': 'Your full name' },
-  ];
-
-  for (var i = 0; i < helpText.length; i++) {
-    var item = helpText[i];
-    var htmlEl = document.getElementById(item.id);
-
-    htmlEl.addEventListener('focus', makeShowHelp(item.help))
+  function makeShowHelp(help) {
+    return function () {// here we put help param in closures
+      showHelp(help);
+    }
   }
-}
 
-setupHelp();
+  function setupHelp() {
+    var helpText = [
+      { 'id': 'email', 'help': 'Your e-mail address' },
+      { 'id': 'name', 'help': 'Your full name' },
+    ];
+
+    for (var i = 0; i < helpText.length; i++) {
+      var item = helpText[i];
+      var htmlEl = document.getElementById(item.id);
+
+      htmlEl.addEventListener('focus', makeShowHelp(item.help))
+    }
+  }
+
+  setupHelp();
+}
 
 // Exemple 4
 
@@ -73,4 +78,25 @@ function ex4() {
   console.dir(fnWithClosure);
 
 }
-ex4()
+
+// Exemple with setTimeout
+
+function ex5WithoutClosure() {
+
+  for (var index = 0; index < 5; index++) { // we can use let to prevent this
+    setTimeout(function () {
+      console.log(index)
+    })
+  }
+}
+// it will output: 5 5 5 5 5
+
+function ex5WithClosure() {
+  for (var index = 0; index < 5; index++) {
+    setTimeout((function (index) {
+      return function () {
+        console.log(index)
+      }
+    })(index), 100 * index)
+  }
+}
